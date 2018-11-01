54962012215261a19c1044bf2b24e17893b36a48
spro1 <spro1@naver.com>
2018-11-01 14:12:16 +0900

    bug fix

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import app.scrap.scrap as scrap
+import app.api.upload as upload
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "storage/file"
+
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def timer():
     namu_urls = scrap.namu_crawler()
     naver_news_urls = scrap.naver_news_crawler()
     dc_urls = scrap.dc_crawler()
     river = scrap.river_temp()
     return render_template('index.html', namu_urls=namu_urls, naver_urls = naver_news_urls, dc_urls = dc_urls, river=river)
+
+
+@app.route('/file/upload', methods=['POST'])
+def file():
+    return jsonify(upload.file_upload(request, UPLOAD_FOLDER))
+
+
+
+
 
 
 if __name__ == '__main__':
