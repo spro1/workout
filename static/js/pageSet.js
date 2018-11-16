c9efceb82b909aa9d06cfa58e553f12d008b788c
spro1 <spro1@naver.com>
2018-11-16 09:57:14 +0900

    디자인 변경

---
+++ static/js/pageSet.js
@@ -0,0 +1,5 @@
+document.getElementById('toggle_click').addEventListener('click', function (e) {
+    var curDom = e.target;
+    selectBar = curDom.parentNode.parentNode;
+    selectBar.classList.toggle('open');
+});
