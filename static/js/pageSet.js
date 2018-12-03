a0ec6400b6cdea4f9883d77aafdb007a680e47c9
spro1 <spro1@naver.com>
2018-12-03 17:44:32 +0900

    merge 충돌

--- static/js/pageSet.js
+++ static/js/pageSet.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-document.getElementById('toggle_click').addEventListener('click', function (e) {
-    var curDom = e.target;
-    selectBar = curDom.parentNode.parentNode;
-    selectBar.classList.toggle('open');
-=======
 /*
 name : pageSet.js
 */
@@ -11,5 +5,4 @@ document.getElementById('toggle_click').addEventListener('click', function (e) {
     var curDom = e.target;
     selectBar = curDom.parentNode.parentNode;
     selectBar.classList.toggle('open');
->>>>>>> 2d59e8dc177bc3601e522e51c4a30e26b37e1ac6
 });
